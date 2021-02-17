d09a10f027edf01dc45a3c33212788be4d32d19b
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-02-17 11:06:57 +0700

    Hari ketiga

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -8,31 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @Environment(\.managedObjectContext) var managedObjectContext
-    
-    @State private var showingAddTodoView: Bool = false
-    var body: some View {
-        NavigationView{
-            List(0..<5){ item in
-                Text("Hello Word")
-            }.navigationBarTitle("Todo",displayMode: .inline)
-            .navigationBarItems(trailing:
-                                    Button(action: {
-                                        self.showingAddTodoView.toggle()
-                                    }){
-                                        Image(systemName: "plus")
-                                    }
-                                    .sheet(isPresented: $showingAddTodoView){
-                                        AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-                                    }
-                                    )
+  
+  @Environment(\.managedObjectContext) var managedObjectContext
+  
+  @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
+  
+  @State private var showingAddTodoView: Bool = false
+  
+  var body: some View {
+    NavigationView{
+      List{
+        ForEach(self.todos, id: \.self){ todo in
+          HStack{
+            Text(todo.name ?? "Unknown")
+            
+            Spacer()
+            
+            Text(todo.priority ?? "Unknown")
+          }
         }
+        .onDelete(perform: deleteTodo)
+        
+      }
+      .navigationBarTitle("Todo", displayMode: .inline)
+      .navigationBarItems(
+        leading: EditButton(),
+        trailing:
+          // shift tab harus di select dulu => buat mindahin ke kiri
+          Button(action: {
+            self.showingAddTodoView.toggle()
+          }){
+            Image(systemName: "plus")
+          }
+        .sheet(isPresented: $showingAddTodoView){
+          AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+        })
     }
+  }
+  
+  private func deleteTodo(at offsets: IndexSet){
+    for index in offsets{
+      let todo = todos[index]
+      managedObjectContext.delete(todo)
+      
+      do{
+        try managedObjectContext.save()
+      } catch{
+        print(error)
+      }
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    return ContentView().environment(\.managedObjectContext, context)
+  }
 }
