ed3e2b688bcda69cc453b7072bb952525c1f3790
muhammadferdiansyah <dandyferdiansyah2801@gmail.com>
2021-02-18 12:35:10 +0700

    Hari keempat

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -8,62 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  @Environment(\.managedObjectContext) var managedObjectContext
-  
-  @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
-  
-  @State private var showingAddTodoView: Bool = false
-  
-  var body: some View {
-    NavigationView{
-      List{
-        ForEach(self.todos, id: \.self){ todo in
-          HStack{
-            Text(todo.name ?? "Unknown")
-            
-            Spacer()
-            
-            Text(todo.priority ?? "Unknown")
-          }
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var
+        todos: FetchedResults<Todo>
+    
+    @State private var showingAddTodoView: Bool = false
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                List{
+                    ForEach(self.todos, id: \.self){ todo in
+                        HStack{
+                            Text(todo.name ?? "Unknown")
+                            
+                            Spacer()
+                            
+                            Text(todo.priority ??  "Unknown")
+                        }
+                    }
+                    
+                    .onDelete(perform: deleteTodo)
+                }
+                .navigationBarTitle("Todo", displayMode: .inline)
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing:
+                        Button(action: {
+                            self.showingAddTodoView.toggle()
+                        }){
+                            Image(systemName: "plus")
+                        }
+                        .sheet(isPresented: $showingAddTodoView){
+                            AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+                        })
+                
+                if todos.count == 0{
+                    EmptyListView()
+                }
+            }
         }
-        .onDelete(perform: deleteTodo)
-        
-      }
-      .navigationBarTitle("Todo", displayMode: .inline)
-      .navigationBarItems(
-        leading: EditButton(),
-        trailing:
-          // shift tab harus di select dulu => buat mindahin ke kiri
-          Button(action: {
-            self.showingAddTodoView.toggle()
-          }){
-            Image(systemName: "plus")
-          }
-        .sheet(isPresented: $showingAddTodoView){
-          AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
-        })
     }
-  }
-  
-  private func deleteTodo(at offsets: IndexSet){
-    for index in offsets{
-      let todo = todos[index]
-      managedObjectContext.delete(todo)
-      
-      do{
-        try managedObjectContext.save()
-      } catch{
-        print(error)
-      }
+    
+    private func deleteTodo(at offsets: IndexSet){
+        for index in offsets{
+            let todo = todos[index]
+            managedObjectContext.delete(todo)
+            
+            do{
+                try managedObjectContext.save()
+            } catch{
+                print(error)
+            }
+        }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    return ContentView().environment(\.managedObjectContext, context)
-  }
+    static var previews: some View {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        return ContentView().environment(\.managedObjectContext, context)
+    }
 }
